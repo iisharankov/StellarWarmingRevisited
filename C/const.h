6c1b05140372aef679b4fdea7373ef78ab859b7b
iisharankov <ivansharankov3@gmail.com>
2022-02-21 13:35:49 +0100

    adds more constants

--- C/const.h
+++ C/const.h
@@ -1,6 +1,9 @@
 #ifndef const_h
 #define const_h
 
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
 #define MASS_E 9.10938291e-31 // electron mass
 #define MASS_P 1.67262178e-27 // proton mass
 #define STEF_BOLT 5.670373e-8 // Stefan-Boltzmann constant
@@ -29,4 +32,16 @@
 #define S 1.0  // error tolerance
 
 
+// Define constants in equations
+#define P_deg_const ((pow(3.0 * pi2, 2.0/3.0) * (HBAR * HBAR)) / (5.0 * MASS_E))
+#define IdealGasFactor  (K / (mu * MASS_P))
+#define dPdp_deg_const ((pow(3.0 * pi2, 2.0/3.0) * HBAR*HBAR) / (3.0 * MASS_P * MASS_E))
+
+#define Kappa_Kes_const (0.02 * (1.0 +  FRAC_X))
+#define Kappa_Kff_const (1.0e24 * ( FRAC_Z + 0.0001))
+#define Kappa_Khmin_const (2.5e-32 * ( FRAC_Z / 0.02))
+
+#define epsilon_epp ((1.07e-7 / 1.0e5) * FRAC_X*FRAC_X )
+#define epsilon_ecno ((8.24e-26 / 1.0e5) * FRAC_X*FRAC_X * 0.03 )
+#define stefanBoltRadii (4.0 * pi * STEF_BOLT)
 #endif
