3473aca78b7f2176cd42a71872d37f7c6cf8c5fc
iisharankov <ivansharankov3@gmail.com>
2022-03-12 11:54:40 +0100

    more mian files as is

--- C/generateHRD.c
+++ C/generateHRD.c
@@ -6,16 +6,12 @@
 #include "starGenerator.h"
 #include "const.h"
 
-#define MAX(x, y) (((x) > (y)) ? (x) : (y))
-
-
-
 
 double flux(double* starVars) {
     // TODO: no self.Rstar index, just using last
 
     double Lum1 = starVars[3];  // Note: Lum = 4pi BOLT * r^2 * T^4
-    double Lum2 = (4.0 * pi * STEF_BOLT * (starVars[5]*starVars[5]) * (starVars[1]*starVars[1]*starVars[1]*starVars[1]));
+    double Lum2 = (stefanBoltRadii * (starVars[5]*starVars[5]) * (starVars[1]*starVars[1]*starVars[1]*starVars[1]));
 
     return (Lum1 - Lum2) / sqrt(Lum1 * Lum2);
 }
@@ -29,13 +25,11 @@ double* fixDensity(double h, double tempCore) {
     double* starA = mainLoop(h, starADensity, tempCore, 0);
     double* starB = mainLoop(h, starBDensity, tempCore, 0);
     double* starC = mainLoop(h, starCDensity, tempCore, 0);
-    // printf("Done making stars\n");
+    
     // Bisection start
     double starCrho = (starADensity + starBDensity) / 2.0;
 
     while ((starBDensity - starADensity) / 2.0 > tol) {
-        // printf("%f\n", (starBDensity - starADensity) / 2.0);
-
         if (flux(starA) * flux(starC) < 0) {
             for (int i = 0; i < 6; i++) { starB[i] = starC[i]; }
             starBDensity = starCDensity;
@@ -60,43 +54,36 @@ double* fixDensity(double h, double tempCore) {
 
 
 void createMainSequence(double numStars, double minCoreTemp, double maxCoreTemp) {
-
-
+    
     FILE * fPtr;  // File pointer to hold reference to our file 
-
     fPtr = fopen("hrData.txt", "a");  // Open file in append mode
     if(fPtr == NULL) { // If NULL, filefopen unsuccessful
         printf("Unable to create file.\n");
         exit(EXIT_FAILURE); // File not created, exit
     }
 
-
-
     double* coreTemps = malloc(numStars*sizeof(double));
-    double stepSize = (maxCoreTemp - minCoreTemp) / numStars;
 
+    // Create stepsize and make list or ranged temperatures
+    double stepSize = (maxCoreTemp - minCoreTemp) / numStars;
     for (int i=0; i < numStars; i++) coreTemps[i] = minCoreTemp + (i * stepSize); 
 
-    double* surfaceTemps = malloc(numStars*sizeof(double));
-    double* surfaceLums = malloc(numStars*sizeof(double));
+    
     
     for (int i=0; i<numStars; i++) {
-        printf("%d\n", i);
         double* curStar = fixDensity(1000.0, coreTemps[i]);
         
-        // Append stuff
-        double denom = (4.0 * pi * STEF_BOLT * curStar[5]*curStar[5]);
-        surfaceTemps[i] = sqrt(sqrt(curStar[3] / denom));
-        surfaceLums[i] = curStar[3];
+        double denom = (stefanBoltRadii * curStar[5]*curStar[5]);
+        double surfaceTemp = sqrt(sqrt(curStar[3] / denom));
 
-
-        // Allocates storage
-        char *dataLine = (char*)malloc( 4 * sizeof(double) ); // (6 * sizeof(double)) for the whitespace
         
-        // Create the line of vectores to save into the file
-        sprintf(dataLine, "%e\t%e", surfaceTemps[i], surfaceLums[i]);
+        // Create the line of vectors to save into the file
+        char *dataLine = (char*)malloc( 8 * sizeof(double) ); // Allocate storage
+        sprintf(dataLine, "%e\t%e\t%e\t%e", surfaceTemp, curStar[3], curStar[5], curStar[2]);
         fputs(dataLine, fPtr); // Write data to file
         fputs("\n", fPtr); // Write new line
+
+        free(dataLine);
     }
 
 
--- C/main.c
+++ C/main.c
@@ -7,31 +7,8 @@
 #include <math.h>
 
 int main() {
-    
-    // double* firstVec = malloc(5*sizeof(double));
-    // firstVec[0] = 1;
-    // firstVec[1] = 2;
-    // firstVec[2] = 3;
-    // firstVec[3] = 4;
-    // firstVec[4] = 5;
+    mainLoop(1000.0, 0.5e3, 1.5e7, 0);
 
-    // double radius = 4;
-    // double h = 4;
-    // double* result = rk4(firstVec, radius, h);
-    // double newRadius = radius + h;
-
-    // for (int i = 0; i < 5; i++) { printf("%e\n",result[i]); }
-    // printf("%e\n", newRadius);
-
-
-    // writeIterationToFile(result, newRadius);
-
-    // mainLoop(1000.0, 0.5e3, 1.5e7, 0);
-
-    createMainSequence(100, pow(10.0, 6.6), pow(10.0, 7.4));
-    // double outputVec[5];
-    // for (int i = 0; i<5; i++) { printf("%e\n",firstVec[i]); }
-    // *outputVec = func(firstVec, radius);
-    // for (int i = 0; i<5; i++) { printf("%e\n",firstVec[i]); }
+    //createMainSequence(100, pow(10.0, 6.6), pow(10.0, 7.4));
     return 0;
 }
