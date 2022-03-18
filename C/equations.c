fb0defeeb0bda648f710ee34f37312e88ca4421d
iisharankov <ivansharankov3@gmail.com>
2022-03-18 01:00:10 +0100

    optimizations to equations for speedup improvements

--- C/equations.c
+++ C/equations.c
@@ -6,23 +6,25 @@
 // Pressures
 double P(const double density, const double temp) {
 
-    // Pressure degenerate - Eqn 5 in the project_description
-    double P_deg = P_deg_const * pow(density / MASS_P, 5.0/3.0);
+    // Degenerate Pressure
+    double dDivMp = density / MASS_P;
+    double P_deg = P_deg_const * cbrt(dDivMp*dDivMp*dDivMp*dDivMp*dDivMp);
 
-    // Pressure Ideal Gas
+    // Ideal Gas Pressure
     double P_ig = (temp * density) * IdealGasFactor;
 
-    // Pressure Radiative
+    // Radiative Pressure
     double P_rad = (RAD_CONST / 3.0) * temp*temp*temp*temp;
 
     return P_deg + P_ig + P_rad;
 }
 
 // Pressure differentials
-double dPdp(const double density, const double temp) {
+static double dPdp(const double density, const double temp) {
 
-    // Degenerate
-    double dPdp_deg = dPdp_deg_const * pow(density / MASS_P, 2.0/3.0);
+    // Degenerate   
+    double dDivMp = density / MASS_P;
+    double dPdp_deg = dPdp_deg_const * cbrt(dDivMp*dDivMp);
     double dPdp_ig = IdealGasFactor * temp;  // Ideal Gas
 
     return dPdp_deg + dPdp_ig;
@@ -31,13 +33,13 @@ double dPdp(const double density, const double temp) {
 
 double dPdT(const double density, const double temp) {
     double dPdT_ig = density * IdealGasFactor;
-    double dPdT_rad = (4.0 / 3.0) * RAD_CONST * temp*temp*temp;
+    double dPdT_rad = (4.0/3.0) * RAD_CONST * temp*temp*temp;
     return dPdT_ig + dPdT_rad;
 }
 
 // Energy generation
 double epsilon(const double density, const double temp) {
-    double rTemp = temp / 1.0e6; // reducedTemp
+    double rTemp = temp / 1.0e6; // reducedTemp        
 
     double epp = epsilon_epp * density * rTemp*rTemp*rTemp*rTemp;
     double ecno = epsilon_ecno * density * pow(rTemp, 19.9);
@@ -47,22 +49,25 @@ double epsilon(const double density, const double temp) {
 
 
 // Opacity
-double Kappa(const double density, const double temp) {
-    // double Kes = 0.02 * (1.0 +  FRAC_X);
-    double Kff = Kappa_Kff_const * pow(density / 1.0e3, 0.7) * pow(temp, -3.5);
-    double Khmin = Kappa_Khmin_const * pow(density / 1.0e3, 0.5) * pow(temp, 9.0);
+double Kappa(double density, double temp) {
+    double tempToPowThreeAndHalf =  temp*temp*temp*sqrt(temp);
+    double reducedDen = density / 1.0e3;
+    double tempToPowNine = temp*temp*temp*temp*temp*temp*temp*temp*temp;
 
-    // return 1/ ( (1.0 / Khmin) + (1.0 / MAX(Kappa_Kes_const, Kff)));
+    double Kff = Kappa_Kff_const * pow(reducedDen, 0.7) / tempToPowThreeAndHalf; 
+    double Khmin = Kappa_Khmin_const * sqrt(reducedDen) * tempToPowNine;
+
     double max_result = MAX(Kappa_Kes_const, Kff);
     return (Khmin * max_result)/(Khmin + max_result);
 }
 
-// Stellar Structure ODEs
+
+// Stellar Structure ODEs //
 
 // dTdr = change in temperature over radius
-double dTdr(const double radius, const double mass, const double density, const double temp, const double lum) {
+double dTdr(const double radius, const double mass, const double density, const double temp, const double lum, double curKappa) {
     // second equation in the set of 5 equations in the project description file
-    double radiative = (3.0 * Kappa(density, temp) * density * lum) / (
+    double radiative = (3.0 * curKappa * density * lum) / (
         16.0 * pi * RAD_CONST * C * (temp * temp * temp) * (radius * radius));
 
     double convection = (1.0 - (1.0 / GAMMA)) * (temp / P(density, temp)) * (
@@ -73,35 +78,39 @@ double dTdr(const double radius, const double mass, const double density, const 
 
 
 // dpdr = change in density over radius
-double dpdr(const double radius, const double mass, const double density, const double temp, const double lum) {
+double dpdr(const double radius, const double mass, const double density, const double temp, const double lum, double curKappa) {
     // First last equation in the set of 5 equations in the project description file
     return -((G * mass * density / (radius*radius)) +
-             dPdT(density, temp) * dTdr(radius, mass, density, temp, lum)) / (dPdp(density, temp));
+             dPdT(density, temp) * dTdr(radius, mass, density, temp, lum, curKappa)) / (dPdp(density, temp));
 }
 
 // dMdr = change in mass over radius
-double dMdr(const double radius, const double density) {
-    // middle last equation in the set of 5 equations in the project description file
-    return 4.0 * pi * (radius * radius) * density;
-}
+// double dMdr(const double radius, const double density) {
+//     // middle last equation in the set of 5 equations in the project description file
+//     return 4.0 * pi * radius * radius * density;
+// }
 
 // DLdr = change in luminosity over radius
 double dLdr(const double radius, const double density, const double temp) {
     // Second last equation in the set of 5 equations in the project description file
-    return dMdr(radius, density) * epsilon(density, temp);
+    const double dMdr = 4.0 * pi * radius * radius * density;
+    return dMdr * epsilon(density, temp);
 }
 
-double dtaudr(const double density, const double temp) {
-    // Last equation in the set of 5 equations in the project description file
-    return Kappa(density, temp) * density;
-}
+// double dtaudr(const double density, const double temp) {
+//     // Last equation in the set of 5 equations in the project description file
+//     return Kappa(density, temp) * density;
+// }
 
 // dPdr = change in pressure over radius
-double dPdr(const double radius, const double mass, const double density) {
-    return -(G * mass * density / (radius * radius));
-}
+// double dPdr(const double radius, const double mass, const double density) {
+//     return -(G * mass * density / (radius * radius));
+// }
 
 // delta(tau) for optical depth limit
-double dtau(const double radius, const double mass, const double density, const double temp, const double lum) {
-    return (Kappa(density, temp) * (density * density)) / (double)(-(dpdr(radius, mass, density, temp, lum)));
+double dtau(const double radius, const double mass, const double density, const double temp, const double lum, double curKappa) {
+    double dpdrSoln = dpdr(radius, mass, density, temp, lum, curKappa);
+    if (dpdrSoln < 0) dpdrSoln *= -1.0; // Take absolute
+
+    return (Kappa(density, temp) * (density * density)) / dpdrSoln;
 }
--- C/equations.h
+++ C/equations.h
@@ -3,19 +3,19 @@
 
 
 double P(const double density, const double temp); // initial implementation of Eulers timestep
-double dPdp(const double density, const double temp); // Pressure differentials
+// static double dPdp(const double density, const double temp); // Pressure differentials
 double dPdT(const double density, const double temp);
 double epsilon(const double density, const double temp); //Energy generation
 double Kappa(const double density, const double temp); // Opacity
 
 // Stellar Structure ODEs
-double dpdr(const double radius, const double mass, const double density, const double temp, const double lum);
-double dTdr(const double radius, const double mass, const double density, const double temp, const double lum);
+double dpdr(const double radius, const double mass, const double density, const double temp, const double lum, double curKappa);
+double dTdr(const double radius, const double mass, const double density, const double temp, const double lum, double curKappa);
 double dMdr(const double radius, const double density);
 
 double dLdr(const double radius, const double density, const double temp);
 double dtaudr(const double density, const double temp);
-double dPdr(const double radius, const double mass, const double density);
-double dtau(const double radius, const double mass, const double density, const double temp, const double lum);  // delta(tau) for optical depth limit
+// double dPdr(const double radius, const double mass, const double density);
+double dtau(const double radius, const double mass, const double density, const double temp, const double lum, double curKappa);  // delta(tau) for optical depth limit
 
 #endif
